e2bd4c98d7eac3f1e09b3b0134c4c2ad85275311
Luk Gastmans <lgastmans@gmail.com>
2026-03-19 08:24:01 +0530

    bug update

--- app/Console/Commands/ImportHerbariumImages.php
+++ app/Console/Commands/ImportHerbariumImages.php
@@ -64,10 +64,14 @@ class ImportHerbariumImages extends Command
             } else {
                 // Try fallback: prepend "F "
                 $fallbackValue = 'F ' . $rawCollection;
+                $this->info($fallbackValue);
                 $normalizedFallback = $this->normalize($fallbackValue);
+                $this->info($normalizedFallback);
 
                 if (isset($lookup[$normalizedFallback])) {
+                    $this->info('set');
                     $herbarium = $lookup[$normalizedFallback];
+                    $this->info($herbarium);
                     $updated = true;
                 }
             }
@@ -81,7 +85,7 @@ class ImportHerbariumImages extends Command
                 continue;
             }
 
-            $herbarium = $lookup[$normalizedFileValue];
+            //$herbarium = $lookup[$normalizedFileValue];
 
             // Avoid duplicates
             if (HerbariumImages::where('herbarium_id', $herbarium->id)
